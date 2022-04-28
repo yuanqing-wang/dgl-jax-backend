c6f2ae9a1f85415395de32a97ac938a2d6f4493e
Yuanqing Wang <wangyq+gh@wangyq.net>
2022-04-28 10:28:40 -0400

    init

--- dgl_jax_backend/init.py
+++ dgl_jax_backend/init.py
@@ -25,59 +25,63 @@ def load_backend(mod_name):
     # (3) Sets up the tensoradapter library path.
     # (4) Import the Python wrappers of the backend framework.  DGL does this last because
     #     it already depends on both the backend framework and the DGL C library.
-    if mod_name == 'pytorch':
-        import torch
-        mod = torch
-    elif mod_name == 'mxnet':
-        import mxnet
-        mod = mxnet
-    elif mod_name == 'tensorflow':
-        import tensorflow
-        mod = tensorflow
+    if mod_name in ["pytorch", "tensorflow", "mxnet"]:
+        from dgl.backend import load_backend
+        load_backend(mod_name)
     elif mod_name == "jax":
         import jax
         mod = jax
+
+        from dgl._ffi.base import load_tensor_adapter # imports DGL C library
+        version = mod.__version__
+        load_tensor_adapter(mod_name, version)
+        logger.debug('Using backend: %s' % mod_name)
+        # mod = importlib.import_module('.%s' % mod_name, __name__)
+        from _backend import jax as _jax
+        mod = _jax
+        # thismod = sys.modules[__name__]
+        import dgl
+        thismod = dgl.backend
+        from dgl.backend import backend as _backend
+        for api in _backend.__dict__.keys():
+            if api.startswith('__'):
+                # ignore python builtin attributes
+                continue
+            if api == 'data_type_dict':
+                # load data type
+                if api not in mod.__dict__:
+                    raise ImportError('API "data_type_dict" is required but missing for'
+                                      ' backend "%s".' % (mod_name))
+                data_type_dict = mod.__dict__[api]()
+                for name, dtype in data_type_dict.items():
+                    setattr(thismod, name, dtype)
+
+                # override data type dict function
+                setattr(thismod, 'data_type_dict', data_type_dict)
+
+                # for data types with aliases, treat the first listed type as
+                # the true one
+                rev_data_type_dict = {}
+                for k, v in data_type_dict.items():
+                    if not v in rev_data_type_dict.keys():
+                        rev_data_type_dict[v] = k
+                setattr(thismod,
+                        'reverse_data_type_dict',
+                        rev_data_type_dict)
+                # log backend name
+                setattr(thismod, 'backend_name', mod_name)
+            else:
+                # load functions
+                if api in mod.__dict__:
+                    _enabled_apis.add(api)
+                    setattr(thismod, api, mod.__dict__[api])
+                else:
+                    setattr(thismod, api, _gen_missing_api(api, mod_name))
+
+            thismod.to_dgl_nd = mod.zerocopy_to_dgl_ndarray
+            thismod.from_dgl_nd = mod.zerocopy_from_dgl_ndarray
+
+            dgl.backend_name = "jax"
+
     else:
         raise NotImplementedError('Unsupported backend: %s' % mod_name)
-
-    from dgl._ffi.base import load_tensor_adapter # imports DGL C library
-    version = mod.__version__
-    load_tensor_adapter(mod_name, version)
-
-    logger.debug('Using backend: %s' % mod_name)
-    mod = importlib.import_module('.%s' % mod_name, __name__)
-    thismod = sys.modules[__name__]
-    for api in backend.__dict__.keys():
-        if api.startswith('__'):
-            # ignore python builtin attributes
-            continue
-        if api == 'data_type_dict':
-            # load data type
-            if api not in mod.__dict__:
-                raise ImportError('API "data_type_dict" is required but missing for'
-                                  ' backend "%s".' % (mod_name))
-            data_type_dict = mod.__dict__[api]()
-            for name, dtype in data_type_dict.items():
-                setattr(thismod, name, dtype)
-
-            # override data type dict function
-            setattr(thismod, 'data_type_dict', data_type_dict)
-
-            # for data types with aliases, treat the first listed type as
-            # the true one
-            rev_data_type_dict = {}
-            for k, v in data_type_dict.items():
-                if not v in rev_data_type_dict.keys():
-                    rev_data_type_dict[v] = k
-            setattr(thismod,
-                    'reverse_data_type_dict',
-                    rev_data_type_dict)
-            # log backend name
-            setattr(thismod, 'backend_name', mod_name)
-        else:
-            # load functions
-            if api in mod.__dict__:
-                _enabled_apis.add(api)
-                setattr(thismod, api, mod.__dict__[api])
-            else:
-                setattr(thismod, api, _gen_missing_api(api, mod_name))
